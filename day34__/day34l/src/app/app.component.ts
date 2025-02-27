cd34301f1bd678ea6e2df5aad175f7be73dc7dfc
itsjonlol <jonathansuherman@gmail.com>
2025-02-27 09:48:48 +0800

    first commit

---
+++ day34__/day34l/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  standalone: false,
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'day34l';
+
+  
+}
