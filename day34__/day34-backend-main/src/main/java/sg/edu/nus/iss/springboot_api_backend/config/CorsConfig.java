17573ad68851ea3ce45da3c4f0ff41d88b8ca792
itsjonlol <jonathansuherman@gmail.com>
2025-02-27 16:35:37 +0800

    added backend

---
+++ day34__/day34-backend-main/src/main/java/sg/edu/nus/iss/springboot_api_backend/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package sg.edu.nus.iss.springboot_api_backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer{
+    
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:4200")
+            .allowedMethods("GET","POST","PUT","DELETE");
+            
+
+        }
+
+        //have allowcredentials and allowheader  and maxage(3600) e.g.
+}
