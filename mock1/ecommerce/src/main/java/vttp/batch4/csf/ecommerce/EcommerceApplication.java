afff9f519865243fffeb9f9c49771c21c72c6169
itsjonlol <jonathansuherman@gmail.com>
2025-03-17 15:16:12 +0800

    added dockerfile

--- mock1/ecommerce/src/main/java/vttp/batch4/csf/ecommerce/EcommerceApplication.java
+++ mock1/ecommerce/src/main/java/vttp/batch4/csf/ecommerce/EcommerceApplication.java
@@ -1,13 +1,31 @@
 package vttp.batch4.csf.ecommerce;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EcommerceApplication {
+public class EcommerceApplication implements CommandLineRunner{
 
+  @Value("${datafile}")
+  private String dataFile;
   public static void main(String[] args) {
     SpringApplication.run(EcommerceApplication.class, args);
   }
 
+  @Override
+  public void run(String... args) throws Exception {
+    try (BufferedReader br = new BufferedReader(new FileReader(dataFile))) {
+      String line;
+      while ((line = br.readLine()) != null) {
+       
+        System.out.println(line);
+    }
+  }
+
+}
 }
